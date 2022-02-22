d3fd6fc8700c42e4155258cca8a6da29b127a0ab
kim-jin-seop <tjq2702@naver.com>
2022-02-22 15:22:54 +0900

    Create App.java

---
+++ code/src/main/java/session3/App.java
@@ -0,0 +1,11 @@
+package session3;
+
+public class App {
+    public static void main(String[] args) {
+        /**
+         * Stream
+         * 1. 데이터를 담고있는 저장소가 아님.
+         *
+         */
+    }
+}
