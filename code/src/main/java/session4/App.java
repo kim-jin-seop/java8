a97d0bfd87be2311f2081c1efbb996e73c2e507f
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-02-25 21:45:30 +0900

    DateTime study setting

---
+++ code/src/main/java/session4/App.java
@@ -0,0 +1,16 @@
+package session4;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App{
+    public static void main(String[] args) {
+        List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        
+    }
+}
---
+++ code/src/main/java/session4/Progress.java
@@ -0,0 +1,19 @@
+package session4;
+
+import java.time.Duration;
+
+public class Progress {
+
+    private java.time.Duration studyDuration;
+
+    private boolean finished;
+
+
+    public Duration getStudyDuration() {
+        return studyDuration;
+    }
+
+    public void setStudyDuration(Duration studyDuration) {
+        this.studyDuration = studyDuration;
+    }
+}
