e613e5493bacb772729a1c38bc1a523c239113e8
kim-jin-seop <33629459+kim-jin-seop@users.noreply.github.com>
2022-02-25 22:16:41 +0900

    optional 사용 개요

--- code/src/main/java/session4/App.java
+++ code/src/main/java/session4/App.java
@@ -1,6 +1,7 @@
 package session4;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class App{
     public static void main(String[] args) {
@@ -11,6 +12,12 @@ public class App{
         springClasses.add(new OnlineClass(4, "spring core", false));
         springClasses.add(new OnlineClass(5, "rest api development", false));
 
+        /**
+         * 1. return값으로만 쓰는것을 권장
+         * 2. Map의 key타입에는 쓰면 안됨(Map의 전제조건이 key값이 null이면 안됨)
+         * 3. Collection을 optional로 감싸지 않아도 됨.
+         */
+        OnlineClass springBoot = new OnlineClass(1, "spring boot", true);
         
     }
 }
--- code/src/main/java/session4/OnlineClass.java
+++ code/src/main/java/session4/OnlineClass.java
@@ -1,5 +1,8 @@
 package session4;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public class OnlineClass {
 
     private Integer id;
@@ -7,6 +10,10 @@ public class OnlineClass {
     private String title;
 
     private boolean closed;
+
+    public Progress progress;
+
+
 
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
@@ -26,6 +33,16 @@ public class OnlineClass {
         return title;
     }
 
+    /**
+     * Optional로 감싸주는 경우는 return 받는 경우에 해주면 된다.(getter)
+     * 만약 null일 수도 있는 경우 -> ofNullable을 사용하면 감싸주면 된다.
+     * 되도록 getter에 넣어라.
+     */
+    public Optional<Progress> getProgress() {
+        // Optional로 감싸서 보내준다.
+        return Optional.ofNullable(progress);
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -36,5 +53,9 @@ public class OnlineClass {
 
     public void setClosed(boolean closed) {
         this.closed = closed;
+    }
+
+    public void setProgress(Progress progress) {
+        this.progress = progress;
     }
 }
